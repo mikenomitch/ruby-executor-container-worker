bde6330bf0d200fdca4f66bdcc2206674d5a052b
Mike Nomitch <mnomitch@cloudflare.com>
2025-03-10 20:17:36 -0700

    Minor tweaks

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
-import { DurableObject } from 'cloudflare:workers';
+import { DurableObject } from "cloudflare:workers";
 
 export default {
-	async fetch(request, env) {
-		const url = new URL(request.url);
+  async fetch(request, env) {
+    const url = new URL(request.url);
 
-		if (url.pathname.startsWith('/code')) {
-			const body = await request.json();
+    if (url.pathname.startsWith("/code")) {
+      const { code, "session-id": sessionId } = await request.json();
+      const id = env.CODE_EXECUTOR.idFromName(sessionId);
+      return env.CODE_EXECUTOR.get(id).execute(code);
+    }
 
-			const sessionId = body['session-id'];
-			const id = env.CODE_EXECUTOR.idFromName(sessionId);
-			const container = env.CODE_EXECUTOR.get(id);
-			return container.executeCode(body['code']);
-		}
-
-		return env.ASSETS.fetch(request);
-	},
+    return env.ASSETS.fetch(request);
+  },
 };
 
 export class CodeExecutor extends DurableObject {
-	constructor(ctx, env) {
-		super(ctx, env);
+  constructor(ctx, env) {
+    super(ctx, env);
 
-		// TODO: enable this once the container is up
-		// ctx.blockConcurrencyWhile(ctx.container.start);
-	}
+    // TODO: enable this once the container is up
+    // ctx.blockConcurrencyWhile(ctx.container.start);
+  }
 
-	async executeCode(code) {
-		// TODO: enable this once the container is up
-		// return await this.ctx.container.getTcpPort(4567).fetch(...);
+  async execute(code) {
+    // TODO: enable this once the container is up
+    // return await this.ctx.container.getTcpPort(4567).fetch(...);
 
-		return await fetch('http://localhost:4567/code', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ code }),
-		});
-	}
+    return await fetch("http://localhost:4567/code", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code }),
+    });
+  }
 }
